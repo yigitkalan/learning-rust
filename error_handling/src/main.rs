5402f3eb4cb3680734ee092931ea119ce83eda06
yigitkalan <sametykalan@gmail.com>
2023-12-05 21:53:54 +0300

    update error handling chapter

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,4 +1,83 @@
+use std::{
+    fs::{File, self},
+    io::{Error, ErrorKind, Read},
+};
+
 fn main() {
-    let v = vec![1,2,3,5,5];
-    println!("{}",v[99]);
+    let _v = vec![1, 2, 3, 5, 5];
+    //this panicks, panics are unrecoverable errors
+    // println!("{}",v[99]);
+
+    let f = File::open("test.txt");
+
+    let f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+
+    //since Result enum type implemnts many functions with closures below would
+    //be a better implementation rather than nested match statements
+    let f = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Problem creating the file: {:?}", error);
+            })
+        } else {
+            panic!("Problem opening the file: {:?}", error);
+        }
+    });
+
+    //unrwap or else takes a closure to get what to do in an error situation
+    //expect takes a string as a message in error situation
+    //just go to definition and be amazed, rust got fucking good docs and source code
+}
+
+//propagation of errors
+fn get_username_from_file(path: &str) -> Result<String, Error> {
+    let f = File::open(path);
+
+    let mut f = match f {
+        Ok(file) => file,
+        Err(err) => return Err(err),
+    };
+
+    let mut s = String::new();
+
+    match f.read_to_string(&mut s) {
+        Ok(_) => Ok(s),
+        Err(e) => Err(e),
+    }
+}
+
+//this is almost the same way of the above function
+fn read_username_from_file(path: &str) -> Result<String, Error> {
+    let mut s = String::new();
+
+    // ? operator takes the value from open function if it's an OK result
+    // or return from the whole function with the Err if that is the result
+    // ? operator also converts the Err type in the result to the Error type in 
+    // the function return type, it does this with from function
+
+    // let mut f = File::open("hello.txt")?;
+    // f.read_to_string(&mut s)?;
+    // Ok(s)
+
+    //Or even shorter way
+    // File::open(path)?.read_to_string(&mut s)?;
+    // Ok(s)
+
+    //ORR EVEN SHORTER WAY
+    //since reading from a file is so common, instead of opening a file than reading from it
+    //this function directly reads from a file and return a string result
+    fs::read_to_string(path)
+
+
+    // ? operator can only be used inside functions that returns a result
+    // so we cannot use it in main function for example
 }
