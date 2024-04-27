71d3a9e565ebb15af022c41e822e890527ec1718
yigitkalan <sametykalan@gmail.com>
2024-04-27 14:43:06 +0300

    add comments to generic

--- 10_lifetimes/src/main.rs
+++ 10_lifetimes/src/main.rs
@@ -67,3 +67,18 @@ where
         y
     }
 }
+
+// The first rule is that each parameter that is a reference gets its own life-
+// time parameter. In other words, a function with one parameter gets one 
+// lifetime parameter: fn foo<'a>(x: &'a i32); a function with two parameters 
+// gets two separate lifetime parameters: fn foo<'a, 'b>(x: &'a i32, y: &'b i32); 
+// and so on.
+
+// The second rule is if there is exactly one input lifetime parameter, that 
+// lifetime is assigned to all output lifetime parameters: fn foo<'a>(x: &'a i32) 
+// -> &'a i32.
+
+// The third rule is if there are multiple input lifetime parameters, but one 
+// of them is &self or &mut self because this is a method, the lifetime of self is 
+// assigned to all output lifetime parameters. This third rule makes methods 
+// much nicer to read and write because fewer symbols are necessary
